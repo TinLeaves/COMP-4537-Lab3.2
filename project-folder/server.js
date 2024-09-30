db9cf595503441d4e173745e385d957a9758cd32
TinLeaves <tinleaves.0@gmail.com>
2024-09-30 00:19:34 -0700

    update to use OOP

--- project-folder/server.js
+++ project-folder/server.js
@@ -10,67 +10,89 @@ const fs = require('fs');
 const message = require('./lang/en/en.js');
 const utils = require('./modules/utils.js');
 
-const server = http.createServer((req, res) => {
-    const parsedUrl = url.parse(req.url, true);
-    const queryObject = parsedUrl.query;
+class Server {
+    constructor(port) {
+        this.port = port;
+        this.server = http.createServer(this.handleRequest.bind(this));
+    }
 
-    if (req.method === 'GET' && parsedUrl.pathname.includes('getDate')) {
-        const name = queryObject.name;
+    start() {
+        this.server.listen(this.port, () => {
+            console.log(`Server is running on port ${this.port}`);
+        });
+    }
+
+    handleRequest(req, res) {
+        const parsedUrl = url.parse(req.url, true);
+        const queryObject = parsedUrl.query;
+
+        if (req.method === 'GET' && parsedUrl.pathname.includes('getDate')) {
+            this.handleGetDate(queryObject.name, res);
+        } else if (req.method === 'GET' && parsedUrl.pathname.includes('writeFile')) {
+            this.handleWriteFile(queryObject.text, res);
+        } else if (req.method === 'GET' && parsedUrl.pathname.includes('readFile')) {
+            const fileName = parsedUrl.pathname.split('/').pop();
+            this.handleReadFile(fileName, res);
+        } else {
+            this.handleNotFound(res);
+        }
+    }
+
+    handleGetDate(name, res) {
         const currentDate = utils.getDate().toString();
         const greetingMsg = `<p style="color: blue;">${message.hello.replace('%1', name)} ${currentDate}</p>`;
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.end(greetingMsg);
-    } else if (req.method === 'GET' && parsedUrl.pathname.includes('writeFile')) {
-        const text = queryObject.text;
-        handleWriteFile(text, res);
-    } else if (req.method === 'GET' && parsedUrl.pathname.includes('readFile')) {
-        const fileName = parsedUrl.pathname.split('/').pop();
-        handleReadFile(fileName, res);
-    } else {
-        res.writeHead(404, { 'Content-Type': 'text/plain' });
-        res.end(`${message.notFound}`);
     }
-});
 
-const handleWriteFile = (text, res) => {
-    const filePath = './readFile/file.txt';
-    if (text) {
-        fs.appendFile(filePath, text + '\n', (err) => {
-            if (err) {
-                res.writeHead(500, { 'Content-Type': 'text/plain' });
-                res.end(`${message.writeError}`);
-            } else {
-                res.writeHead(200, { 'Content-Type': 'text/plain' });
-                res.end(`${message.appended} ${text}`);
-            }
-        });
-    } else {
-        res.writeHead(404, { 'Content-Type': 'text/plain' });
-        res.end(`${message.writeError}`);
-    }
-};
-
-const handleReadFile = (fileName, res) => {
-    const filePath = `./readFile/${fileName}`;
-    fs.access(filePath, fs.constants.F_OK, (err) => {
-        if (err) {
-            res.writeHead(404, { 'Content-Type': 'text/plain' });
-            res.end(`${message.notFound} : ${fileName}`);
-        } else {
-            fs.readFile(filePath, 'utf-8', (err, data) => {
+    handleWriteFile(text, res) {
+        const filePath = './readFile/file.txt';
+        if (text) {
+            fs.appendFile(filePath, text + '\n', (err) => {
                 if (err) {
-                    res.writeHead(500, { 'Content-Type': 'text/plain' });
-                    res.end(`${message.readError}`);
+                    this.sendError(res, 500, message.writeError);
                 } else {
-                    res.writeHead(200, { 'Content-Type': 'text/plain' });
-                    res.end(data);
+                    this.sendResponse(res, 200, `${message.appended} ${text}`);
                 }
             });
+        } else {
+            this.sendError(res, 404, message.writeError);
         }
-    });
-};
+    }
+
+    handleReadFile(fileName, res) {
+        const filePath = `./readFile/${fileName}`;
+        fs.access(filePath, fs.constants.F_OK, (err) => {
+            if (err) {
+                this.sendError(res, 404, `${message.notFound} : ${fileName}`);
+            } else {
+                fs.readFile(filePath, 'utf-8', (err, data) => {
+                    if (err) {
+                        this.sendError(res, 500, message.readError);
+                    } else {
+                        this.sendResponse(res, 200, data);
+                    }
+                });
+            }
+        });
+    }
+
+    handleNotFound(res) {
+        this.sendError(res, 404, message.notFound);
+    }
+
+    sendResponse(res, statusCode, message) {
+        res.writeHead(statusCode, { 'Content-Type': 'text/plain' });
+        res.end(message);
+    }
+
+    sendError(res, statusCode, message) {
+        res.writeHead(statusCode, { 'Content-Type': 'text/plain' });
+        res.end(message);
+    }
+}
 
 const PORT = 8000;
-server.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+const server = new Server(PORT);
+server.start();
+
